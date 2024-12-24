c03ce10ad8bb9d87a2310224b3311ec9da9fcbe1
NAGARAJAN E <98300427+NAGARAJANELAN@users.noreply.github.com>
2024-12-24 17:59:19 +0530

    cleanup

--- palmCropper.py
+++ palmCropper.py
@@ -9,19 +9,18 @@ def cropPalm(img):
         hand = hands[0]
         x, y, w, h = hand["bbox"]
 
-        # cropped_hand = img[y-20:y+20+h, x-20:x+20+w] #works for entire hand
         try:
             handType = hand["type"]
             if handType == "Right":
                 cropped_hand = img[
                     y + int(h * 0.43) : y + int(h * 0.93),
                     x + int(w * 0.15) : x + int(w * 0.76),
-                ]  # right hand ig
+                ]  # right hand
             else:
                 cropped_hand = img[
                     y + int(h * 0.43) : y + int(h * 0.93),
                     x + int(w * 0.24) : x + int(w * 0.84),
-                ]  # left hand ig
+                ]  # left hand
             return cropped_hand
         except:
             print("An exception occurred while cropping palm")
